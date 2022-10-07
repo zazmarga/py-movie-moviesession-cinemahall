0fd89ef38f81b1bd081aa91d94702f05477751ab
danylott <d.tuytuyshkin@gmail.com>
2022-10-07 13:52:33 +0300

    Added flake8 plugins

--- db/models.py
+++ db/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -12,5 +12,5 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
