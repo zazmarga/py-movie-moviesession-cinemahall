4042f169c89a84127a04f271b36e4c471663c2ca
Marharyta Zazulia <zazmargo@gmail.com>
2024-09-18 00:55:23 -0300

    Revert "'Solution'"
    This reverts commit 88fbbb541998d819c8ed71f160cbc7e3fc3c0742.

--- db/models.py
+++ db/models.py
@@ -14,37 +14,3 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
-
-
-class Movie(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    actors = models.ManyToManyField(Actor, related_name="movies")
-    genres = models.ManyToManyField(Genre, related_name="movies")
-
-    def __str__(self) -> str:
-        return self.title
-
-
-class CinemaHall(models.Model):
-    name = models.CharField(max_length=255)
-    rows = models.IntegerField()
-    seats_in_row = models.IntegerField()
-
-    def __str__(self) -> str:
-        return self.name
-
-    @property
-    def capacity(self) -> int:
-        return self.rows * self.seats_in_row
-
-
-class MovieSession(models.Model):
-    show_time = models.DateTimeField()
-    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-
-    def __str__(self) -> str:
-        format_time = "%Y-%m-%d %H:%M:%S"
-        return (f"{self.movie.title} "
-                f"{self.show_time.strftime(format_time)}")
