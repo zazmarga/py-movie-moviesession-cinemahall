de7565235ca212be6fe3c0fbc0158b9605a4f7fc
Marharyta Zazulia <zazmargo@gmail.com>
2024-09-18 21:01:40 -0300

    used movie_session_by_id

--- services/movie_session.py
+++ services/movie_session.py
@@ -36,7 +36,7 @@ def update_movie_session(session_id: int,
                          movie_id: int = None,
                          cinema_hall_id: int = None
                          ) -> None:
-    movie_session = MovieSession.objects.get(id=session_id)
+    movie_session = get_movie_session_by_id(session_id)
     if show_time:
         movie_session.show_time = show_time
     if movie_id:
@@ -47,5 +47,5 @@ def update_movie_session(session_id: int,
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    movie_session = MovieSession.objects.get(id=session_id)
+    movie_session = get_movie_session_by_id(session_id)
     movie_session.delete()
